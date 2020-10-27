7870d0a9aa8d97a78a6667d30c5bf9f33a26f3c4
Patrick Njoli <patimjoli@gmail.com>
2020-10-27 17:16:19 +0100

    added a route handler

--- app.js
+++ app.js
@@ -34,13 +34,59 @@ const server = http.createServer((req, res) => {
   req.on("end", () => {
     buffer += decoder.end();
 
-    console.log(buffer);
+    //Choose the handler this request should go to, if not found default to not found handler
+    let choosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : router.notfound;
 
-    //send response
-    res.end("welcome");
+    //Construct a data obj to send to the choosen handler
+    const data = {
+      trimmedPath: trimmedPath,
+      queryStringOBJ: queryStringOBJ,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+    //Route the request to the handler specified in the router
+    choosenHandler(data, (statusCode, payload) => {
+      //Use the statusCode called back by the handler, or default to 200
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+
+      //Use the payload called backe by the handler, or defaualt to {}
+      payload = typeof payload === "object" ? payload : {};
+
+      //Cconvert payload to string
+      const payloadSting = JSON.stringify(payload);
+
+      res.setHeader("Content-type", "application/json");
+
+      res.writeHead(statusCode);
+      console.log(statusCode, " ", payloadSting);
+      //send response
+      res.end(payloadSting);
+    });
   });
 });
 
 server.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+//define the handlers
+const handlers = {};
+
+//Sample handler
+handlers.sample = (data, callback) => {
+  callback(500, { msg: "in the sample route" });
+};
+//not found handler
+handlers.notfound = (data, callback) => {
+  callback(404, { msg: "not found" });
+};
+// define request router
+
+let router = {
+  sample: handlers.sample,
+  notfound: handlers.notfound,
+};
