5d1888b91d61a54f5a302ce3f2c416b09ca9daa7
Patrick Njoli <patimjoli@gmail.com>
2020-10-27 09:39:24 +0100

    parsed the request url and trimmed it

--- app.js
+++ app.js
@@ -4,10 +4,21 @@
  */
 
 const http = require("http");
+const url = require("url");
 
 //the server should respond to all request with a string
 const server = http.createServer((req, res) => {
-  res.end("Hello World");
+  //Get the url and parse it
+  const parsedURL = url.parse(req.url, true);
+
+  //get the path
+  const path = parsedURL.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  console.log(trimmedPath);
+
+  //send response
+  res.end("welcome");
 });
 
 server.listen(3000, () => {
